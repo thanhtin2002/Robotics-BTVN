2b0129743cf9f668c183320cc9529963a75ebab6
thanhtin2002 <thanhtin16062002@gmail.com>
2022-09-22 21:49:27 +0700

    second commit

--- ROBOTICS/Flower4.m
+++ ROBOTICS/Flower4.m
@@ -1,8 +1,8 @@
 l1=50;l2=40;
 syms t;
 for t=0:0.01:2*pi
-    X=40+10*cos(t);
-    Y=40+10*sin(t);
+    X=cos(2*t)*cos(t);
+    Y=cos(2*t)*sin(t);
     c2=(X^2+Y^2-l1^2-l2^2)/2*l1*l2;
     s2=sqrt(abs(1-c2^2));
     t2=atan(s2,c2);
@@ -11,13 +11,7 @@ for t=0:0.01:2*pi
     t1=atan(s1,c1);
     px=l1*cos(t1)+l2*cos(t1+t2);
     py=l1*sin(t1)+l2*sin(t1+t2);
-    subplot(2,1,1);
-    plot(t,t1*180/pi,'--o',t,t2*180/pi,'-o');
-    pause(0.1);
-    xlabel('t');
-    ylabel('Rotation');
     hold on;
-    subplot(2,1,2);
     plot(X,Y,'--o');
     xlabel('x(cm)');
     ylabel('y(cm)');
